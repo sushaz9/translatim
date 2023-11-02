c574e5665fc81fa8fe1b1f839087acc87494d48f
sushaz9 <susannahelisabeth1@gmail.com>
2023-11-02 14:27:17 +0000

    translationdata

--- server/server.js
+++ server/server.js
@@ -8,6 +8,27 @@ const axios = require("axios");
 
 app.get("/", (_, response) => response.json("Root route for translatim."));
 
-// add your endpoints here
+// add your endpoints here - /translate?word=hello&from=en&to=es
+
+app.get("/translate", async (request, response) => {
+  // word to from
+  /*const word = request.query.word;
+  const from = request.query.from;
+  const to = request.query.to;
+  or destructure in just one line of code */
+
+  const { word, from, to } = request.query;
+
+  // make api call
+  const API = `https://api.mymemory.translated.net/get?q=${word}&langpair=${from}|${to}`;
+  const res = await axios.get(API);
+
+  const wrangledData = {
+    translation: res.data.responseData.translatedText,
+    match: res.data.responseData.match,
+  };
+
+  response.json(wrangledData);
+});
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
